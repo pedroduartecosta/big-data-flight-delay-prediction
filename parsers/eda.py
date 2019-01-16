18c4d1bfc2374f91aab6c0c9cc3b25a106a70a08
Carol <carolina.mneves@gmail.com>
2019-01-16 17:58:50 +0100

    Images and parsers

---
+++ parsers/eda.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np 
+import matplotlib as plt
+import matplotlib.pyplot as pyplot
+import seaborn as sns
+from sklearn.neighbors import KernelDensity
+
+import ast, json
+
+
+#--------------------------------------CORRELATION MATRIX--------------------------------------#
+def matrixCorr(df):
+	corr = df.corr()
+
+	# Generate a mask for the upper triangle
+	#mask = np.zeros_like(corr, dtype=np.bool)
+	#mask[np.triu_indices_from(mask)] = True
+
+	# Set up the matplotlib figure
+	f, ax = plt.pyplot.subplots(figsize=(11, 9))
+
+	# Generate a custom diverging colormap
+	cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+	# Draw the heatmap with the mask and correct aspect ratio
+	sns.heatmap(corr, cmap=cmap, annot=True, vmax=.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5})
+	pyplot.show()
+
+def scatterPlot(df):
+	sns.regplot(x=df["ArrDelay"], y=df["DepTime"])
+	pyplot.show()
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+pd.set_option('display.max_columns', 19)
+sns.set(style="white")
+sns.set(color_codes=True)
+
+df = pd.read_csv('./UPM-BigData-Spark/flightdelaypredictor/data/2008.csv', sep=",")
+
+delCol = ['ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn', 'Diverted', 'CarrierDelay', 
+		'WeatherDelay', 'NASDelay', 'SecurityDelay', 'LateAircraftDelay']
+df = df.drop(delCol, axis=1)
+
+#print(df.shape)
+#print(df.info())
+#print(df.describe())
+#print(df.corr(method ='pearson'))
+#print(df.head())
+
+#matrixCorr(df)
+scatterPlot(df)
+
+
+
+
+
+#https://www.linkedin.com/pulse/generating-correlation-heatmaps-seaborn-python-andrew-holt
+#https://towardsdatascience.com/hitchhikers-guide-to-exploratory-data-analysis-6e8d896d3f7e
